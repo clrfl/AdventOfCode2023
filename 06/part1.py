b7b02db3d5a40a7488dc6a5a847dc7282d941214
clrfl <clrfl@users.noreply.github.com>
2023-12-06 13:28:03 +0100

    6part1

--- 06/part1.py
+++ 06/part1.py
@@ -1,2 +1,31 @@
 f = open("input.txt", "r")
-file = f.read().split("\n\n")
+file = f.readlines()
+
+time = file[0].strip()
+dist = file[1].strip()
+
+while "  " in time or "  " in dist:
+    time = time.replace("  ", " ")
+    dist = dist.replace("  ", " ")
+
+time = time.split(" ")[1:]
+dist = dist.split(" ")[1:]
+
+pairs = []
+for i in range(len(time)):
+    pairs.append((time[i], dist[i]))
+
+results = []
+for pair in pairs:
+    wins = 0
+    for mmps in range(int(pair[0])+1):
+        rem_time = int(pair[0]) - mmps
+        if mmps * rem_time > int(pair[1]):
+            wins += 1
+    results.append(wins)
+
+total = 1
+for entry in results:
+    total *= entry
+
+print(total)
