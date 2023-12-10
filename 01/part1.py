c248dd16f9e2b433be50c94a91dffd28dbf52299
clrfl <clrfl@users.noreply.github.com>
2023-12-10 18:01:02 +0100

    1part1 different solution

--- 01/part1.py
+++ 01/part1.py
@@ -1,12 +1,7 @@
-f = open("input.txt", "r")
+import string
+
 result = 0
-for line in f.readlines():
-    line = line.strip()
-    length = len(line)
-    for i in range(length):
-        if (line[i]).isnumeric():
-            last = line[i]
-        if (line[length - i - 1]).isnumeric():
-            first = line[length - i - 1]
-    result += int(first + last)
+for line in open("input.txt", "r").readlines():
+    line = line.strip().strip(string.ascii_lowercase)
+    result += int(line[0] + line[-1])
 print(result)
