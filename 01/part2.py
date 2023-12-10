5bbf326754fc478b51b365d7a8e50294a8c23eb2
clrfl <clrfl@users.noreply.github.com>
2023-12-10 18:05:13 +0100

    1part2 different solution

--- 01/part2.py
+++ 01/part2.py
@@ -1,16 +1,13 @@
+import string
+
 f = open("input.txt", "r")
 dictionary = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6",
-        "seven": "7", "eight": "8", "nine": "9"}
+              "seven": "7", "eight": "8", "nine": "9"}
 result = 0
 for line in f.readlines():
     line = line.strip()
     for entry in dictionary:
         line = line.replace(entry, entry + dictionary[entry] + entry)
-    length = len(line)
-    for i in range(length):
-        if (line[i]).isnumeric():
-            last = line[i]
-        if (line[length - i - 1]).isnumeric():
-            first = line[length - i - 1]
-    result += int(first + last)
+    line = line.strip().strip(string.ascii_lowercase)
+    result += int(line[0] + line[-1])
 print(result)
